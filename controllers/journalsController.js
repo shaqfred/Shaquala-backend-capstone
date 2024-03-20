ec995334e8686d7d4fb7bc55d1f5a00093a53ca8
shaquala <96317236+shaqfred@users.noreply.github.com>
2024-03-20 12:57:20 -0400

    complete first get route and data shows in postman

--- controllers/journalsController.js
+++ controllers/journalsController.js
@@ -4,8 +4,17 @@ const journals = express.Router();
 
 const { getAllJournal } = require("../query/journal.js");
 
-journals.get("/", (request, response) => {
-  response.status(200).json({ message: "Journalfly Home Page" });
+// journals.get("/", (request, response) => {
+//   response.status(200).json({ message: "Journalfly Home Page" });
+// });
+
+journals.get("/", async (request, response) => {
+  try {
+    const allJournals = await getAllJournal();
+    response.status(200).json(allJournals);
+  } catch (error) {
+    response.status(404).json({ message: error });
+  }
 });
 
 module.exports = journals;
