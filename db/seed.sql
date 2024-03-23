ec7677314478d35f00fa9ebd0acf99ef9c08fbdc
shaquala <96317236+shaqfred@users.noreply.github.com>
2024-03-23 11:13:53 -0400

    added addition data to keep working on thefunctions as I preparefor validations

--- db/seed.sql
+++ db/seed.sql
@@ -1,10 +1,13 @@
 \c journal_dev;
 
 INSERT INTO journal(
-    'journal_entry', 'journal_mood', 'journal_affirmation', 'start_date')
+    journal_entry, journal_mood, journal_affirmation)
 VALUES(
 'text','sad','It is always good to let someone know how you feel'),
 ('message','happy','Today is a great day'),
-('written', 'angry','Today is a day for meditation');
+('written', 'angry','Today is a day for meditation'),
+('read', 'exhausted', 'Rest is always good, we need to rest, Bath may do the trick'),
+('draw', 'surprised', 'Receiving a gift for personal gain is not good yet unexpected gives a different feel to being surprised'
+);
 
 
